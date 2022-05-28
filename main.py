6e4ff63c0ce8b9bd59d185a075a5a896893da957
Attila213 <attilamolnr@outlook.hu>
2022-05-28 12:40:12 +0200

    add clock and set framerate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from cgi import print_arguments
-import pygame,sys,os
+import pygame,sys,os,time
 import functions as fun
 pygame.init()
+clock = pygame.time.Clock()
 
 WINDOW_SIZE = [700,700]
 screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
@@ -13,14 +13,17 @@ imgs = fun.image_loader()
 
 map = fun.map_generation((50,10),8)
 
-
+frame = 0
 run = True
 while run:
+    frame += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
     
-
-        
+    
+    
+    clock.tick(120)    
     screen.blit(pygame.transform.scale(display,WINDOW_SIZE),(0,0))
     pygame.display.update()
+    

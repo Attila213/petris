b2561dd5e3036b0c7ccdd78ba9c54bcfb633510e
Attila213 <attilamolnr@outlook.hu>
2022-05-28 10:29:25 +0200

    create functions and render the display

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame,sys,os
-
+import functions as fun
 pygame.init()
 
-screen = pygame.display.set_mode((500,500),0,32)
+WINDOW_SIZE = [700,700]
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
+display = pygame.Surface((100,100))
+
 pygame.display.set_caption("PETRIS")
 
 
@@ -12,7 +15,8 @@ while run:
         if event.type == pygame.QUIT:
             pygame.quit()
             
-            
     
+    fun.printstg()
     
+    screen.blit(pygame.transform.scale(display,WINDOW_SIZE),(0,0))
     pygame.display.update()

2f089d0e7e90b30fde66eedbc55da146ef676f7e
Attila213 <attilamolnr@outlook.hu>
2022-05-28 10:54:41 +0200

    make map_generation function

--- functions.py
+++ functions.py
@@ -1,2 +1,11 @@
 import pygame,sys,os
 
+def map_generation(surface,startpos,size):
+    map = []
+    x= startpos[0]
+    y= startpos[1]
+    for i in range(10):
+        for j in range(15):
+            r = pygame.Rect(x+ i*(size),y+j*(size),size,size)
+            map.append(r)
+    return map
